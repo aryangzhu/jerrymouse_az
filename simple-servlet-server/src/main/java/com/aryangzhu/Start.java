31836340aad7e61282596dcf078d37c100812a79
liulei <1935314139@qq.com>
2025-01-03 02:23:50 +0800

    feat:简单servlet代码

--- simple-servlet-server/src/main/java/com/aryangzhu/Start.java
+++ simple-servlet-server/src/main/java/com/aryangzhu/Start.java
@@ -1,7 +1,24 @@
 package com.aryangzhu;
 
-public class Start {
-    public static void main(String[] args) throws Exception {
+import com.aryangzhu.connector.HttpConnector;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public class Start {
+
+    static Logger logger= LoggerFactory.getLogger(Start.class);
+    public static void main(String[] args) throws Exception {
+        try (HttpConnector connector = new HttpConnector()) {
+            for (;;) {
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            logger.error(e.getMessage(), e);
+        }
+        logger.info("jerrymouse http server was shutdown.");
     }
 }
--- simple-servlet-server/src/main/java/com/aryangzhu/connector/HttpExchangeAdapter.java
+++ simple-servlet-server/src/main/java/com/aryangzhu/connector/HttpExchangeAdapter.java
@@ -1,34 +1,42 @@
 package com.aryangzhu.connector;
 
 import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.URI;
 
 public class HttpExchangeAdapter implements HttpExchangeRequest,HttpExchangeResponse{
+
+    final HttpExchange exchange;
+
+    public HttpExchangeAdapter(HttpExchange exchange) {
+        this.exchange = exchange;
+    }
+
     @Override
     public String getRequestMethod() {
-        return null;
+        return this.exchange.getRequestMethod();
     }
 
     @Override
     public URI getRequestURI() {
-        return null;
+        return this.exchange.getRequestURI();
     }
 
     @Override
     public Headers getResponseHeaders() {
-        return null;
+        return this.exchange.getResponseHeaders();
     }
 
     @Override
     public void sendResponseHeaders(int statusCode, long contentLength) throws IOException {
-
+        this.exchange.sendResponseHeaders(statusCode, contentLength);
     }
 
     @Override
     public OutputStream getResponseBody() {
-        return null;
+        return this.exchange.getResponseBody();
     }
 }
